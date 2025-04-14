cf6d176ec64fa58dba9bde45760224c4f95c2cf3
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-04-14 11:41:54 -0300

    feat: adicionando as rotas de 'Help' à aplicação

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import type { FastifyInstance } from 'fastify'
 import { DangerRouter } from './routers/danger-router'
+import { HelpRouter } from './routers/help-router'
 import { UserRouter } from './routers/user-router'
 
 const userRouter = new UserRouter()
 const dangerRouter = new DangerRouter()
+const helpRouter = new HelpRouter()
 
 export async function configRoutes(app: FastifyInstance) {
   app.register(userRouter.routes, { prefix: '/users' })
   app.register(dangerRouter.routes, { prefix: '/dangers' })
+  app.register(helpRouter.routes, { prefix: '/helps' })
 }
